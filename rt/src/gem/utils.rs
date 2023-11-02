34a4a86d7c41ec87e8294059d7847f6bfc09c88c
healingdrawing <healingdrawing@gmail.com>
2023-11-02 21:08:19 +0200

    wip check_mat. ray_x_mat, ray_x_ball implemented

--- rt/src/gem/utils.rs
+++ rt/src/gem/utils.rs
@@ -29,6 +29,49 @@ impl Gem {
 
   }
 
+  /// intersection of ray and sphere.
+  /// 
+  /// The first one from ray origin, to forward direction, along the ray vector.
+  /// 
+  /// If there is no intersection than return Dot::maximum(), to avoid brainfuck
+  pub fn ray_x_ball(ray: &Mat, center:&Dot, radius:f64) -> Dot {
+    // vector from the ball's center to the ray's origin. Just for case without unit autoconvertion, so Spear is not used. Just components separately
+    let x = ray.origin.x - center.x;
+    let y = ray.origin.y - center.y;
+    let z = ray.origin.z - center.z;
+
+    // Coefficients for the quadratic equation
+    let a = ray.normal.x.powi(2) + ray.normal.y.powi(2) + ray.normal.z.powi(2);
+    let b = 2f64 * (x * ray.normal.x + y * ray.normal.y + z * ray.normal.z);
+    let c = x * x + y * y + z * z - radius.powi(2);
+
+    // Calculate the discriminant
+    let d = b * b - 4.0 * a * c;
+
+    // If the discriminant is negative, there are no intersections
+    if d < 0.0 { return Dot::maximum() }
+
+    // Calculate the two possible solutions for t
+    let t1 = (-b - d.sqrt()) / (2.0 * a);
+    let t2 = (-b + d.sqrt()) / (2.0 * a);
+
+    // Choose the closer to ray origin (which is zoom point) intersection point
+    let p1 = Dot::new(
+      ray.origin.x + t1 * ray.normal.x,
+      ray.origin.y + t1 * ray.normal.y,
+      ray.origin.z + t1 * ray.normal.z,
+    );
+
+    let p2 = Dot::new(
+      ray.origin.x + t2 * ray.normal.x,
+      ray.origin.y + t2 * ray.normal.y,
+      ray.origin.z + t2 * ray.normal.z,
+    );
+
+    if p1.d_dot(&ray.origin) < p2.d_dot(&ray.origin) {p1} else {p2}
+    
+  }
+
   /// convert radians to degrees
   pub fn degrees(angle_radians: f64) -> f64 {
     angle_radians * 180.0 / std::f64::consts::PI
--- rt/src/tracer/pixel.rs
+++ rt/src/tracer/pixel.rs
@@ -83,6 +83,7 @@ impl Scene {
             good_to_trace,
           );
         }
+        //todo: implement
         Objects::Ball { color, position, radius, } => {}
         Objects::Box { color, position, size, } => {}
         Objects::Roll { color, position, radius, height, } => {}
@@ -103,18 +104,19 @@ impl Scene {
     good_to_trace: &Vec<Objects>
   ) -> (RGB, Dot) {
     let light_position = Dot::from_array(self.light.position);
+    let object = object.clone(); // to avoid borrow checker
     // drop object with index, which is incoming object
     let other_objects:Vec<Objects> = good_to_trace.iter().enumerate().filter(|(i, _)| *i != index).map(|(_, o)| o.clone()).collect();
     
     // find the object intersection point and color, or set color to background, and intersection to must far point, to avoid any rust "magic"
     let (mut obj_pixel_color, mut obj_pixel_position) = match object {
       Objects::Mat { color, position, normal } => {
-        let mat_origin = Dot::from_array(*position);
-        let mat_normal = Spear::from_array(*normal);
+        let mat_origin = Dot::from_array(position);
+        let mat_normal = Spear::from_array(normal);
         let xyz = Gem::ray_x_mat(&ray, &Mat::new(mat_origin, mat_normal));
         (
           RGB::power_affected(
-            *color,
+            color,
             xyz,
             light_position,
             RGB::from_array(&self.light.color),
@@ -128,16 +130,51 @@ impl Scene {
     };
     
     let (mut pixel_color, mut pixel_position) = (RGB::background(), Dot::maximum());
-
+    
     if obj_pixel_position.d_dot(&light_position) <= self.light.power{
       (pixel_color, pixel_position) = (obj_pixel_color, obj_pixel_position);
     }
-
-    // here, build the ray to light source, iterate the other objects , and
+    
+    // here, build the ray to light source, iterate the other_objects , and
     // if there is some other intersection closer to light source than
     // obj_pixel_position, than implement dark_side method to slow down the color
+    // and break the loop, because there is no need to check other objects
+    
+    let ray_to_light = Mat::new(
+      obj_pixel_position,
+      Spear::pp(
+        &[
+          obj_pixel_position,
+          Dot::from_array(self.light.position),
+        ]
+      )
+    );
+    
+    for object in other_objects {
+      match object {
+        Objects::Mat { color, position, normal } => {
+          let mat_origin = Dot::from_array(position);
+          let mat_normal = Spear::from_array(normal);
+          let xyz = Gem::ray_x_mat(&ray_to_light, &Mat::new(mat_origin, mat_normal));
+          if xyz.d_dot(&light_position) < obj_pixel_position.d_dot(&light_position) {pixel_color = pixel_color.dark_side(); break;}
+        }
+        Objects::Ball { color, position, radius } => {
+          let ball_center = Dot::from_array(position);
+          let xyz = Gem::ray_x_ball(&ray_to_light, &ball_center, radius);
+          if xyz.d_dot(&light_position) < obj_pixel_position.d_dot(&light_position) {pixel_color = pixel_color.dark_side(); break;}
+        }
+        Objects::Box { color, position, size } => {
+          //todo: implement
+        }
+        Objects::Roll { color, position, radius, height } => {
+          //todo: implement
+        }
+      }
+    }
+    
+    // compare with nearest_position, and return the nearest one (with color)
+    if pixel_position.d_dot(&ray.origin) < nearest_position.d_dot(&ray.origin) { (pixel_color, pixel_position) } else { (old_color, nearest_position) }
 
-    (pixel_color, pixel_position) //todo: remove. dev gap
   }
   
 }
