6c0b4e0162a7f5ef3218bf8dbe6501aa0d51c843
jo-eman <eckermanjohannes@gmail.com>
2023-11-07 13:15:19 +0200

    refactor for performance with rayon par_iter multithreading

--- rt/src/tracer/trace.rs
+++ rt/src/tracer/trace.rs
@@ -1,45 +1,29 @@
 use super::scene::Scene;
 
+use rayon::prelude::*;
+use std::sync::Arc;
+
 impl Scene {
-  
-  pub fn trace(&self) -> Vec<u8> {
-    // objects of the scene to iterate over
-    let objects = &self.objects;
-    // camera of the scene
-    let camera = &self.camera;
-    // light of the scene
-    let light = &self.light;
+    pub fn trace(&self) -> Vec<u8> {
+        let camera = &self.camera;
+        let width = camera.width;
+        let height = camera.height;
 
-    // width of the camera
-    let width = camera.width;
-    // height of the camera
-    let height = camera.height;
+        // Pre-calculate values for good_to_trace and avoid cloning inside the loop
+        let good_to_trace = Arc::new(self.good_to_trace(&self.objects));
+        let good_to_trace_ref = Arc::as_ref(&good_to_trace);
 
-    // data to be returned
-    let mut data = vec![0u8; width * height * 3];
-
-    // clear the objects, remove the objects that are not good to trace
-    let good_to_trace = self.good_to_trace(objects);
-
-    // iterate over the pixels of the camera to create rays
-    for row in 0..height {
-      for col in 0..width {
-        // index of the pixel in the data array
-        let index = ((row * width + col) * 3) as usize;
-
-        // find the pixel color for the ray
-        let color = self.pixel_color(row, col, &good_to_trace);
-
-        // set the pixel color in the data array
-        data[index] = color.r;
-        data[index + 1] = color.g;
-        data[index + 2] = color.b;
-
-      }
+        // Flatten the iteration over pixels
+        (0..(width * height))
+            .into_par_iter()
+            .map(|i| {
+                let row = i / width;
+                let col = i % width;
+                // Use the reference directly without cloning
+                let color = self.pixel_color(row, col, good_to_trace_ref);
+                vec![color.r, color.g, color.b] // Return the pixel color as a flat vector
+            })
+            .flatten() // Flatten the results into a single vector
+            .collect() // Collect into a Vec<u8> and return
     }
-
-    data
-    
-  }
-
 }
